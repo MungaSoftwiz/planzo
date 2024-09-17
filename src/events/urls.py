e21a695f9b970ed4c7c2de80166a850d9502c90e
TheOrbMaverick <enangita@gmail.com>
2024-09-17 13:51:55 +0100

    stripe checkout problems

--- src/events/urls.py
+++ src/events/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import CreateCheckoutSession
+# from views import CreateCheckoutSessionsView
 #Added 
 app_name = 'home'
 
@@ -8,6 +10,11 @@ urlpatterns = [
     path('<int:id>/', views.eventDetailView, name='eventDetail'),
     path('<int:id>/edit/', views.eventEditView, name='eventEdit'),
     path('<int:id>/delete/', views.eventDeleteView, name='eventDelete'),
+    path('create-checkout-session/', CreateCheckoutSession, name='createCheckoutSession'),
+    # path('create-checkout-session/', views.CreateCheckoutSessionsView.as_view(), name='CreateCheckoutSession'),
+    # path('product-landing-page/', ProductLandingPageView.as_view(), name='prroductionLandingPage')
+    # path('charge/', views.charge, name="charge"),
+    # path('success/<str:args>/', views.successMsg, name="success"),
 ]
 
 # url patterns for events"
